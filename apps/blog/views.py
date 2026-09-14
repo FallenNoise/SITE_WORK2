6d9e6197a56fe6ad1c969c53c07125e740008797
Бойбородов Илья <fflash08@mail.ru>
2026-09-14 12:24:16 +0300

    feat(posts/list): Реализовать функционал получения списка постов.

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def home_page(request):
     posts = Post.objects.all().order_by('-created_at')[:3]
     return render(request, 'pages/index.html', {'posts': posts})
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'pages/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'pages/post_detail.html', {'post': post})
