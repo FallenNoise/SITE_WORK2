ec09fce306b24768a9451042fd2261fefbf04849
Бойбородов Илья <fflash08@mail.ru>
2026-09-14 12:56:48 +0300

    style(forms): Перевести названия полей формы на русский язык

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'text']
+        labels = {
+            'title': 'Заголовок',
+            'text': 'Текст',
+        }
